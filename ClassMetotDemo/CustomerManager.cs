424ce419147fd7937340380823c17a2ecd165854
TurkCoderv1 <turkbahr19@hotmail.com>
2021-03-16 16:35:13 +0300

    init

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        public void AddCustomer(Customer customer)
+        {
+            customers.Add(customer);
+        }
+        public void ShowCustomers ()
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Müşteri ID:" +customer.Id);
+                Console.WriteLine("Müşteri adı ve soy adı:" + customer.Name+""+ customer.Surname);
+                
+            }
+           
+        }
+            
+        public void DeleteCustomer(Customer customer)
+        {
+            customers.Remove(customer);
+        } 
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            CustomerManager customerManager = new CustomerManager();
+            customer1.Id = 52;
+            customer1.Name = "Melih";
+            customer1.Surname = "Sivri";
+            customerManager.AddCustomer(customer1);
+            customerManager.ShowCustomers();
+
+            
+        }
+    }
+}
